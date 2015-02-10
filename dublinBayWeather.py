ef83935a095f281bfff321c6185d392c71264212
irwingarry <irwingarry@gmail.com>
2015-02-10 15:19:16 +0000

    Added Weather Scraper
    @DublinBayBouy

---
+++ dublinBayWeather.py
@@ -0,0 +1,28 @@
+import tweepy
+import json
+import re
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+
+#unique authentication codes to access twitter api
+ckey = 'pkCWTxTCX76fULhSSPZMeaOXp'
+csecret = 'CQYW4iXTtM29yhtE6TL2ZWLYut33J3gGiftiE0WVHG7npuPFQw'
+atoken = '321691375-uFP1WTIiRMNblmE2so8tNLrgYwFrB4v19wKXAOHt' 
+asecret =  'wfJga59aQTQUHtYTJQDcu913hisjYYioKi0uE9Kg6JozB'
+
+#authorisation handler
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+#access to api
+api = tweepy.API(auth)
+
+status_list = api.user_timeline('dublinbaybuoy',count = 1)
+status = status_list[0]
+#converts tweet to json then a dict
+json_tweet = json.dumps(status._json)
+tweet = json.loads(json_tweet)
+text = tweet["text"]
+print text
