227b30fcc45542835d0958f7f7d3faa6c9353e98
irwingarry <irwingarry@gmail.com>
2015-02-17 15:17:14 +0000

    db for twitter
    now adds tweet to a mongo db database

--- dublinBayWeather.py
+++ dublinBayWeather.py
@@ -1,6 +1,7 @@
 import tweepy
 import json
 import re
+from pymongo import Connection
 from tweepy import Stream
 from tweepy import OAuthHandler
 from tweepy.streaming import StreamListener
@@ -25,4 +26,23 @@ status = status_list[0]
 json_tweet = json.dumps(status._json)
 tweet = json.loads(json_tweet)
 text = tweet["text"]
-print text
+
+#creates a map of floats from numbers in the text using regular expressions
+m = map(float, re.findall(r'[-+]?[0-9]*\.?[0-9]', text))
+
+#setup db
+con = Connection()
+#creates database if it doesnt exist
+db = con.weather_database
+
+stats= db.stats
+
+stats.insert({'avg_wind': m[0], 'gust':m[1], 'wind_dir':m[2],
+	'wave_height':m[3],'wave_period':m[4],'water_temp':[5],
+	'day':m[6],'month':m[7],'year':m[8],'hour':m[9],
+	'minute':m[10],'second':m[11]})
+collection = stats.find()
+for stat in collection:
+	print stat
+
+
